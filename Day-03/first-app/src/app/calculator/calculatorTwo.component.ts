cd9023709d189c6ec07d162a34e043b3aa7da9a4
Magesh <tkmagesh77@gmail.com>
2018-07-25 14:42:26 +0530

    Calculator-2 implementation

---
+++ Day-03/first-app/src/app/calculator/calculatorTwo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CalculatorModel } from './CalculatorModel';
+
+@Component({
+	selector : 'app-calculator-2',
+	templateUrl : './calculatorTwo.component.html'
+})
+export class CalculatorTwoComponent{
+	model : CalculatorModel = new CalculatorModel();
+	operator : string = 'add';
+
+	
+}
