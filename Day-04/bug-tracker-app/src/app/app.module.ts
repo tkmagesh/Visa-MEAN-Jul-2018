9f8e25b270116e783c994df879602171575a58c6
Magesh <tkmagesh77@gmail.com>
2018-07-26 09:34:15 +0530

    refactoring to bugStatsComponent

--- Day-04/bug-tracker-app/src/app/app.module.ts
+++ Day-04/bug-tracker-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
+import { BugStatsComponent } from './bugTracker/views/bugStats.component';
 
 @NgModule({
   declarations: [
     AppComponent
     , BugTrackerComponent
+    , BugStatsComponent
   ],
   imports: [
     BrowserModule
