4bb32484ef55033ed45433928daf612744caacc6
Magesh <tkmagesh77@gmail.com>
2018-07-31 14:02:45 +0530

    async seq problem

--- Day-07/04-webappserver-refactored/serveNotFound.js
+++ Day-07/04-webappserver-refactored/serveNotFound.js
@@ -1,4 +1,5 @@
 module.exports = function(res){
+	console.log('[@serveNotFound] serving 404');
 	res.statusCode = 404;
 	res.end();
 }
--- Day-07/04-webappserver-refactored/serveStatic.js
+++ Day-07/04-webappserver-refactored/serveStatic.js
@@ -16,6 +16,14 @@ module.exports = function(req, res){
 			res.end();
 			return;
 		}
-		fs.createReadStream(resourcePath).pipe(res);
+		//fs.createReadStream(resourcePath).pipe(res);
+		let stream = fs.createReadStream(resourcePath);
+		stream.on('data', function(chunk){
+			console.log('[@serveStatic] serving one chunk of data to response');
+			res.write(chunk);
+		});
+		stream.on('end', function(){
+			res.end();
+		});
 	}
 }
